8e4896832863e3c6f3e21b8f955ca826c4381066
Oleg-Kuznetov <oikuznetsov.ru>
2024-04-20 00:32:08 +0500

    load

---
+++ pages/settings.py
@@ -0,0 +1,15 @@
+# валидные значения переменных
+valid_phone = ""
+valid_login = ""
+valid_pass = ""
+valid_email = ""
+valid_pass_email = ""
+valid_surname = ""
+
+#невалидные значения переменных
+invalid_phone = "+79998887654"
+invalid_login = "rtkid_1697217915887"
+invalid_account = "004947376552"
+invalid_pass = "Qwerty-12345"
+invalid_email = "45zf7w7zgu@ezztt.com"
+invalid_pass_email = "KOI!@12345"
